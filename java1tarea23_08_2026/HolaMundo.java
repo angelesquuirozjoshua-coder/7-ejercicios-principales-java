dba67118dc843eebe543c52deaf0b213047c934e
angelesquuirozjoshua-coder <angelesquuirozjoshua@gmail.com>
2026-08-23 19:44:51 -0600

    Add files via upload

---
+++ java1tarea23_08_2026/HolaMundo.java
@@ -0,0 +1,7 @@
+//Ejemplo: Hola Mundo En Java
+public class HolaMundo {
+public static void main(String[] args) {
+System.out.println("¡Hola, Mundo!");
+System.out.println("Esta es la clase");
+}
+}
